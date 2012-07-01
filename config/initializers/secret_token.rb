023ae858aea158b7cabaf80a9c15c193326e71b9
cedricdlb <cdlb.git@gmail.com>
2012-06-30 23:58:04 -0700

    first commit, of: rails new fb_tools

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbTools::Application.config.secret_token = 'de36a01e3eb691981bff84fba8f846b5cdefd846f5dd64897b39e5756090162530b3e4f418c0084b8a8938aeac185449e113642e4111258d3aad3ab30837d66f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FbTools::Application.config.session_store :cookie_store, key: '_fb_tools_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FbTools::Application.config.session_store :active_record_store
